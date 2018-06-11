c4b7d42031d5f0719f1a858e87a650ef32a1b9ee
jlukenoff <jlukenoff@gmail.com>
2018-06-11 12:22:35 -0700

    Completed Stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,48 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  //check for primitive types
+  if (typeof obj === 'number') {
+    return obj.toString();
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (obj === null) {
+    return 'null';
+  } else if (typeof obj === 'boolean') {
+    return obj.toString();
+  } else if (Array.isArray(obj)) {
+    if (obj.length === 0) {
+      return '[]';
+    }
+    //declare a string
+    let str = '[';
+    //iterate through array
+    for (let i = 0; i < obj.length; i++) {
+      //forEach value
+      str += stringifyJSON(obj[i]);
+      if (i < obj.length - 1) {
+        str += ',';
+      }
+    }
+    str += ']';
+    return str;  
+  } else if (obj.constructor === Object) {
+    //grab object keys
+    let keys = Object.keys(obj);
+    let str = '';
+    
+    //check for empty object
+    if (keys.length === 0) {
+      return '{}';
+    }
+
+    //iterate through Object
+    for (let key of keys) {
+      if (obj[key] !== undefined && !(obj[key] instanceof Function)) {
+        str += '"' + key + '":' + stringifyJSON(obj[key]) + ',';
+      }
+    }
+    
+    return '{' + str.slice(0, str.length - 1) + '}';    
+  }
 };
