60dd0c2fe4493848f817070cc0d69e6c363675b6
jlukenoff <jlukenoff@gmail.com>
2018-06-11 20:25:40 -0700

    saving changes to parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,32 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  //object handler
+  if (json[0] === '{') {
+    let newObj = {};
+    let inString = false;
+    let inKey = true;
+    let str = '';
+    for (let i = 0; i < json.length; i++) {
+      //starts string sequence
+      if (json[i] === '"' && !inString) {
+        inString = true;
+      //ends string sequence
+      } if (json[i] === '"' && inString) {
+        inString = false;
+        newObj[str] = 
+        str = '';
+      } if (inString) {
+        str += json[i];
+      } if (json[i] === ':') {
+        inKey = false;
+      }
+    }
+  // array handler
+  } else if (json[0] === '[') {
+    let newArr = [];
+    if (json === '[]') {
+      return newArr;
+    }
+  }
 };
